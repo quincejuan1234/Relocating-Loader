4a24f37e874face761ab06324f86d6347054ee14
Kerm-of-the-Crop <maponte0220@gmail.com>
2025-11-29 15:26:45 -0500

    Filled out relocSicXE.c

--- src/relocSicXE.c
+++ src/relocSicXE.c
@@ -1,4 +1,9 @@
 #include "relocSicXE.h"
+#include "memory.h"
+#include "objFile.h"
+#include "util.h"
+
+#include <stddef.h>
 
 /** 
  * Relocation logic for SIC/XE object programs.
@@ -19,5 +24,56 @@
 
 
 void relocateSicXE(objFile *obj, uint32_t reloc) {
-    // TODO: implement SIC/XE relocation
+    if (!obj) {
+        fatal("relocateSicXE: NULL objFile pointer");
+    }
+
+    uint32_t oldStart = obj->header.startAddress;
+    int32_t  R        = (int32_t)reloc - (int32_t)oldStart;
+
+    memInit();
+
+    for (size_t i = 0; i < obj->textCount; i++) {
+        textRecord *t = &obj->textRecords[i];
+        
+        for (uint32_t j = 0; j < t->length; j++) {
+            uint32_t addr = t->address + j;
+            memWriteByte(addr, t->bytes[j]);
+        }
+    }
+
+    for (size_t i = 0; i < obj->modCount; i++) {
+        modRecord *m = &obj->modRecords[i];
+
+        uint32_t word = memReadWord(m->address);
+
+        uint8_t halfBytes = m->lengthNibbles;
+        uint8_t bits      = (uint8_t)(halfBytes * 4);
+
+        if (bits == 0 || bits > 24) {
+            fatal("relocateSicXE: invalid modification length in nibbles");
+        }
+
+        uint32_t mask = (1u << bits) - 1u;
+
+        uint32_t field = word & mask;
+
+        if (m->sign == '+') {
+            field += (uint32_t)R;
+        }
+        else if (m->sign == '-') {
+            field -= (uint32_t)R;
+        }
+        else {
+            fatal("relocateSicXE: invalid sign in modification record (expected '+' or '-')");
+        }
+
+        field &= mask;
+
+        word = (word & ~mask) | field;
+
+        word &= 0xFFFFFFu;
+
+        memWriteWord(m->address, word);
+    }
 }
